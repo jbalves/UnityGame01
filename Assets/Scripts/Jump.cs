729c341c6f97d33dcf16356a7a1831b2a260d9e3
Jeferson Barros <soldier.jhonson@gmail.com>
2017-02-11 21:46:38 -0400

    Initial project

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space )){
+			rb.AddForce(0,thrust,0 , ForceMode.Impulse);
+		}
+	}
+}
